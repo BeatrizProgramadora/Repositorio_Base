5a139e8a5b7c7f7b4b2cf82e6c111b15ceadef19
BeatrizProgramadora <biamazoninha@gmail.com>
2025-09-10 14:19:57 -0300

    Delete Modulo 1/Condicionais/adicionar_filmes.py

--- Modulo 1/Condicionais/adicionar_filmes.py
+++
@@ -1,9 +0,0 @@
-quantidade_de_filmes= int(input('digite quantos filmes quer adicionar: '))
-contador=1
-filmes=[]
-while contador<=quantidade_de_filmes:
-    filme= input(f'digite o nome do filme {contador}: ')
-    filmes.append(filme) 
-    contador= contador+1
-
-print(filmes)
