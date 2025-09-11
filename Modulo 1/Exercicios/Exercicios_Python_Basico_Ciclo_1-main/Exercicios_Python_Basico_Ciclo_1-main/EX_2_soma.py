d5e00ec13744cff44b03940af5fb3dcda3a9aab7
BeatrizProgramadora <biamazoninha@gmail.com>
2025-09-11 16:26:16 -0300

    Add files via upload

--- Modulo 1/Exercicios/Exercicios_Python_Basico_Ciclo_1-main/Exercicios_Python_Basico_Ciclo_1-main/EX_2_soma.py
+++ Modulo 1/Exercicios/Exercicios_Python_Basico_Ciclo_1-main/Exercicios_Python_Basico_Ciclo_1-main/EX_2_soma.py
@@ -8,3 +8,6 @@
 
 # ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
 
+num1=float(input('Digite um número: '))
+num2=float(input('Digite outro número: '))
+print(f'A soma entre {num1} e {num2} é: {num1+num2} ')
